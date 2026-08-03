6f8c6262b11263ced9636d9358b2e339769a3a28
TAYUN <507028106@qq.com>
2026-08-03 17:14:20 +0800

    feat: 添加物料节点的拖拽和缩放功能

--- src/editor/canvas/index.vue
+++ src/editor/canvas/index.vue
@@ -1,41 +1,117 @@
 <script setup lang="ts">
 import { createNode, getMaterialComponent } from '@/materials'
 import type { MaterialSchema } from '@/materials/types'
+import { useEditorStore } from '@/stores/editor'
+import { storeToRefs } from 'pinia'
+import Moveable, { type OnDrag, type OnResize } from 'vue3-moveable'
 
 defineOptions({
   name: 'CanvasRoot',
 })
 
-const nodes = ref<MaterialSchema[]>([])
+const editorStore = useEditorStore()
+
+const { nodes, selectedNode } = storeToRefs(editorStore)
+
+const moveableRef = useTemplateRef('moveable')
+
+const selectedTarget = shallowRef<HTMLElement>()
+
+const vm = getCurrentInstance()
 
 function onDrop(e: DragEvent) {
   const data = e.dataTransfer.getData('schema')
-  console.log('data', data)
   const node = createNode(JSON.parse(data))
-  // 生成uuid
-  // node.id = crypto.randomUUID()
+  // 组件中心在鼠标落点
+  node.layout.x = e.offsetX - node.layout.width / 2
+  node.layout.y = e.offsetY - node.layout.height / 2
+  editorStore.addNode(node)
+  // 开启放入就选中
+  editorStore.selectNode(node.id)
+  nextTick(() => {
+    selectedTarget.value = vm.proxy.$el.querySelector(`[data-node-id='${node.id}']`)
+  })
+}
 
-  nodes.value.push(node)
+function getNodeStyle(node: MaterialSchema) {
+  return {
+    width: node.layout.width + 'px',
+    height: node.layout.height + 'px',
+    left: node.layout.x + 'px',
+    top: node.layout.y + 'px',
+  }
+}
+function onSelect(node: MaterialSchema, e: MouseEvent) {
+  // 这里记一下笔记为什么用e.currentTarget 而不是 e.target，为什么要断言
+  selectedTarget.value = e.currentTarget as HTMLElement
+  editorStore.selectNode(node.id)
+
+  nextTick(() => {
+    // 解决第一次选中不能拖动的问题
+    moveableRef.value.dragStart(e)
+  })
+}
+function onDrag(e: OnDrag) {
+  // 这里记录笔记 为什么这里要手动设置css？为了手动更新
+  selectedTarget.value.style.left = e.left + 'px'
+  selectedTarget.value.style.top = e.top + 'px'
+
+  selectedNode.value.layout.x = e.left
+  selectedNode.value.layout.y = e.top
+}
+
+function onResize(e: OnResize) {
+  selectedTarget.value.style.width = e.width + 'px'
+  selectedTarget.value.style.height = e.height + 'px'
+
+  selectedNode.value.layout.width = e.width
+  selectedNode.value.layout.height = e.height
+  // 记录这里为什么要调用onDrag？解决往左缩放却往右边缩放不符合预期的问题
+  onDrag(e.drag)
+}
+function onClearSelected() {
+  editorStore.clearSelected()
+  selectedTarget.value = null
 }
 </script>
 
 <template>
   <div class="canvas-root container">
-    <div class="canvas-stage" @dragover.prevent @drop="onDrop">
-      <div v-for="node in nodes" :key="node.id">
+    <div class="canvas-stage" @dragover.prevent @drop="onDrop" @mousedown.self="onClearSelected">
+      <div
+        class="canvas-node"
+        v-for="node in nodes"
+        :key="node.id"
+        :style="getNodeStyle(node)"
+        :data-node-id="node.id"
+        @mousedown="onSelect(node, $event)"
+      >
         <component :is="getMaterialComponent(node.type)" :schema="node"></component>
       </div>
     </div>
+    <Moveable
+      ref="moveable"
+      :target="selectedTarget"
+      :draggable="true"
+      :resizable="true"
+      :origin="false"
+      @drag="onDrag"
+      @resize="onResize"
+    ></Moveable>
   </div>
 </template>
 
 <style scoped lang="scss">
 .canvas-root {
   .canvas-stage {
+    position: relative;
     width: 900px;
     height: 600px;
     background: bg-mix(40);
     margin: 100px;
+    .canvas-node {
+      position: absolute;
+    }
   }
 }
 </style>
--- src/stores/editor.ts
+++ src/stores/editor.ts
@@ -1,3 +1,4 @@
+import type { MaterialSchema } from "@/materials/types";
 import { defineStore } from "pinia";
 
 export const useEditorStore = defineStore('editor', () => {
@@ -7,7 +8,34 @@ export const useEditorStore = defineStore('editor', () => {
     property: true,
   })
 
+  const nodes = ref<MaterialSchema[]>([])
+
+  const selectedNodeId = ref()
+
+  const selectedNode = computed(() => {
+    return nodes.value.find((node) => node.id === selectedNodeId.value)
+  })
+
+  function addNode(node: MaterialSchema) {
+    nodes.value.push(node)
+  }
+
+  function selectNode(id: string) {
+    selectedNodeId.value = id
+  }
+
+  function clearSelected() {
+    selectedNodeId.value = null
+  }
+
+
   return {
     panelVisible,
+    nodes,
+    selectedNode,
+    selectNode,
+    selectedNodeId,
+    addNode,
+    clearSelected,
   }
 })
